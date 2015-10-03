e72ba642a7371be455bf8dd9f6ed6630a8b9f934
bluepichu <github@bluepichu.com>
2015-10-03 18:56:54 -0400

    Initial commit to client

---
+++ client/client.py
@@ -0,0 +1,20 @@
+from __future__ import print_function # uhhh... what version of python are we using here?
+import click # I'll worry about this later
+
+def new_account():
+	pass
+
+def deposit():
+	pass
+
+def widthraw():
+	pass
+
+def check_balance():
+	pass
+
+def print_error(*objs):
+    print("WARNING: ", *objs, file=sys.stderr)
+
+if __name__ == "__main__":
+	main()
