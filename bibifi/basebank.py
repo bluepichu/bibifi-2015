ec832fc43c446cca35c68b801715474807c950c4
Strikeskids <github@strikeskids.com>
2015-10-03 18:15:09 -0400

    Included bank interface

---
+++ bibifi/basebank.py
@@ -0,0 +1,40 @@
+from abc import ABCMeta, abstractmethod
+
+'''
+The client instantiates the communication protocol as a BaseBank and calls methods on that
+The server network handler calls the methods on the implementer of BaseBank that actually stores the data
+
+Requires valid inputs (need to be checked by calling functions)
+Throw error on communication failure
+Return falsy value for failed operation
+Return truthy value for successful operation (possibly contents of keycard)
+'''
+
+class BaseBank(metaclass=ABCMeta):
+    '''
+    Returns keycard contents to be saved
+    '''
+    @abstractmethod
+    def create_account(name):
+        pass
+
+    '''
+    Returns True on success
+    '''
+    @abstractmethod
+    def deposit(name, keycard, amount):
+        pass
+
+    '''
+    Return True on success
+    '''
+    @abstractmethod
+    def withdraw(name, keycard, amount):
+        pass
+
+    '''
+    Returns balance (Currency) on success
+    '''
+    @abstractmethod
+    def check_balance(name, keycard):
+        pass
