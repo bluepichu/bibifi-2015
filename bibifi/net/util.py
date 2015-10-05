e0168693aeb8826e70b6942e0ecbaaf0121f7283
Strikeskids <github@strikeskids.com>
2015-10-05 10:54:32 -0400

    Added protocolbank

--- bibifi/net/util.py
+++ bibifi/net/util.py
@@ -5,3 +5,10 @@ def generate_nonce():
     bits = getrandbits(64)
     nonce = struct.pack('>Q', bits)
     return nonce
+
+def check_for_timeout(tracker):
+    try:
+        tracker.wait(timeout=10)
+        return True
+    except NotDone as e:
+        return False
