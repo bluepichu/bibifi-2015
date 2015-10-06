e99300f7d2774b5ea9ae19f4c05cd23910d8d2e6
Strikeskids <github@strikeskids.com>
2015-10-06 11:48:51 -0400

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='Bibifi 2015',
+    version='0.1',
+    author='Corwin de Boor',
+    author_email='github@strikeskids.com',
+    packages=['bibifi'],
+)
