c4dc8e1854a70cffd0d91822e3487dd554e60d5f
Strikeskids <github@strikeskids.com>
2015-10-07 16:58:53 -0400

    Add bank handler and modify banksocket to be compatible

--- bibifi/bank/banksocket.py
+++ bibifi/bank/banksocket.py
@@ -1,37 +1,77 @@
 import socket
 import threading
 import socketserver
+import sys
+import signal
 
-import bibifi.net.packet as packet
-import bibifi.net.protocol as protocol
+from queue import Queue
 
-class ThreadedHandler(socketserver.BaseRequestHandler): 
+from bibifi.net.packet import read_packet
+from bibifi.net import protocol
+from bibifi.bank.bankhandler import BankRequestStage, BankRequest
+
+class ThreadedHandler(socketserver.BaseRequestHandler):
+    def __init__(self, handler, auth_keys):
+        self.result_queue = Queue(max_size=1)
+        self.finished = False
+        self.handler = handler
+        self.auth_keys = auth_keys
+
+    @classmethod
+    def initializer_with_args(cls, *args):
+        def init():
+            return cls(*args)
+        return init
+
+    def bank_request(self, type, data, stage):
+        self.handler.requests.push(BankRequest(self, type, data, stage))
+
     def handle(self):
-        print("Incoming")
-        parse_packet = packet.read_packet(self.request)
-        packet_type = parse_packet.read_number(4)
-        if packet_type > 0 and packet_type < len(protocol.methods):
-            method = protocol.methods[packet_type]
+        finish = None
+        try:
+            req_packet = read_packet(self.request)
+            packet_type = req_packet.read_number(1)
+
+            if 0 < packet_type < len(protocol.methods):
+                method = protocol.methods[packet_type]
+            else:
+                raise IOError('Invalid method')
+
+            valid, data = method.recv_req(req_packet)
+
+            if valid:
+                req_packet.verify_or_raise()
+
+                self.bank_request(method.name, data, BankRequestStage.start)
+                finish = BankRequestStage.finish_success
+                result = self.result_queue.get()
+            else:
+                result = False
+
+            res_packet = method.send_res(req_packet, result, self.auth_keys)
+            self.request.sendall(res_packet.finish())
+        except IOError as e:
+            print('protocol_error')
+            print(e, file=sys.stderr)
+            sys.stdout.flush()
+            self.request.close()
+            finish = BankRequestStage.finish_fail
         else:
-            #EINV: INVALID COMMAND
-            return
-        valid, data = method.recv_req(parse_packet)
-        if not valid:
-            #EINV: INVALID DATA
-            return
-        print(valid, data)
+            self.finished = True
+            if finish != None:
+                self.bank_request(method.name, data, finish)
 
 class ThreadedServer(socketserver.ThreadingMixIn, socketserver.TCPServer):
     pass
 
-def listen(host, port):
-    print("Listening")
-    server = ThreadedServer((host, port), ThreadedHandler)
-    server_thread = threading.Thread(target = server.serve_forever)
+def listen(host, port, bank_handler, auth_keys):
+    server = ThreadedServer((host, port),
+        ThreadedHandler.initializer_with_args(bank_handler, auth_keys))
+    server_thread = threading.Thread(target=server.serve_forever)
     server_thread.daemon = True
     server_thread.start()
 
-if __name__ == "__main__":
-    listen("127.0.0.1", 3000)
-    while (True):
-        pass
+    def handle_terminate():
+        server_thread.stop()
+
+    return handle_terminate
