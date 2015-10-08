81272785f51493707e00b1ef3f243d0fe88cea68
Strikeskids <github@strikeskids.com>
2015-10-08 09:39:16 -0400

    Add argument parsing to server

--- bibifi/bank/server.py
+++ bibifi/bank/server.py
@@ -1,12 +1,20 @@
 from bibifi.bank import banksocket, bankhandler
 from bibifi.authfile import Keys
+from bibifi import validation, argparser
 
 import sys
 
 def main():
+    parser = argparser.ThrowingArgumentParser()
+    parser.add_argument('-p', metavar='<port>', type=int, help='The port to listen on', default=3000)
+    parser.add_argument("-s", metavar="<auth-file>", type=str, help="The auth file path.  (Default: bank.auth)", default="bank.auth")
+    
     try:
-        # TODO read cl arguments
-        # TODO validate cl arguments
+        args = parser.parse_args()
+
+        if not validation.validate_file(args.s) or not validation.validate_port(args.p):
+            print('Invalid arguments', file=sys.stderr)
+            exit(255)
 
         start_server(port, auth_file_path)
     except Exception as e:
