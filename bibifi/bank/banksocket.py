6c4e90cdc585c383d624204289be37c7e2f6057e
Strikeskids <github@strikeskids.com>
2015-10-08 21:41:26 -0400

    Add banksocket tests

--- bibifi/bank/banksocket.py
+++ bibifi/bank/banksocket.py
@@ -12,10 +12,11 @@ from bibifi.bank.bankhandler import BankRequestStage, BankRequest
 
 class ThreadedHandler(socketserver.BaseRequestHandler):
     def __init__(self, handler, auth_keys):
-        self.result_queue = Queue(max_size=1)
+        self.result_queue = Queue(maxsize=1)
         self.finished = False
         self.handler = handler
         self.auth_keys = auth_keys
+        self.finish = None
 
     @classmethod
     def initializer_with_args(cls, *args):
@@ -26,40 +27,53 @@ class ThreadedHandler(socketserver.BaseRequestHandler):
     def bank_request(self, type, data, stage):
         self.handler.requests.push(BankRequest(self, type, data, stage))
 
-    def handle(self):
-        finish = None
-        try:
-            req_packet = read_packet(self.request)
-            packet_type = req_packet.read_number(1)
+    def read_packet():
+        return read_packet(self.request)
 
-            if 0 < packet_type < len(protocol.methods):
-                method = protocol.methods[packet_type]
-            else:
-                raise IOError('Invalid method')
+    def get_method(self, req_packet):
+        packet_type = req_packet.read_number(1)
+
+        if 0 < packet_type < len(protocol.methods):
+            return protocol.methods[packet_type]
+        else:
+            raise IOError('Invalid method')
+
+    def send_bank(self, method, data):
+        self.bank_request(method.name, data, BankRequestStage.start)
+        self.finish = BankRequestStage.finish_success
+
+    def recv_bank(self):
+        return self.result_queue.get()
+
+    def send_packet(self, res_packet):
+        self.request.sendall(res_packet.finish())
+
+    def handle(self):
+        try:
+            req_packet = self.read_packet()
+            method = self.get_method(req_packet)
 
             valid, data = method.recv_req(req_packet)
 
             if valid:
+                self.send_bank(method, data)
                 req_packet.verify_or_raise()
-
-                self.bank_request(method.name, data, BankRequestStage.start)
-                finish = BankRequestStage.finish_success
-                result = self.result_queue.get()
+                result = self.recv_bank()
             else:
                 result = False
 
             res_packet = method.send_res(req_packet, result, self.auth_keys)
-            self.request.sendall(res_packet.finish())
+            self.send_packet(res_packet)
         except IOError as e:
             print('protocol_error')
             print(e, file=sys.stderr)
             sys.stdout.flush()
             self.request.close()
-            finish = BankRequestStage.finish_fail
-        else:
+            self.finish = BankRequestStage.finish_fail
+        finally:
             self.finished = True
-            if finish != None:
-                self.bank_request(method.name, data, finish)
+            if self.finish != None:
+                self.bank_request(method.name, data, self.finish)
 
 class ThreadedServer(socketserver.ThreadingMixIn, socketserver.TCPServer):
     pass
